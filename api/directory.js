ff6fa57c487f93261e28f4f0f7db9249b2eb9b90
whoffmandesign <whoffmandesign@gmail.com>
2026-03-22 17:15:55 -0400

    Update directory.js

--- api/directory.js
+++ api/directory.js
@@ -36,10 +36,8 @@ export default async function handler(req, res) {
     const peopleData = await peopleRes.json();
     let people = peopleData.items || [];
     if (people.length > 0) {
-      const pa = people[0].PersonAccount && people[0].PersonAccount[0];
-      const acct = pa && pa.Account;
-      console.log('ACCOUNT_KEYS:', acct ? Object.keys(acct) : 'no account');
-      console.log('SUBSCRIPTIONS:', JSON.stringify(acct && acct.Subscriptions, null, 2));
+      console.log('METADATA:', JSON.stringify(peopleData.metadata));
+      console.log('PEOPLE_COUNT:', people.length);
     }
     const total = peopleData.metadata?.total || people.length;
 
